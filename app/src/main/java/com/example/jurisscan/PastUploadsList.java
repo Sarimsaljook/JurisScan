2f1821ef8b8de8206dc5f136631d770a4671f8f0
Sarimsaljook <80625774+Sarimsaljook@users.noreply.github.com>
2024-06-29 18:31:13 -0400

    Add Past Uploaded Files List Activity

---
+++ app/src/main/java/com/example/jurisscan/PastUploadsList.java
@@ -0,0 +1,14 @@
+package com.example.jurisscan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PastUploadsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_past_uploads_list);
+    }
+}
