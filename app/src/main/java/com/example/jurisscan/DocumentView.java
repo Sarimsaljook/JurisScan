82c37b8a52a0091be5b331e48c09c25bfe567682
Sarimsaljook <80625774+Sarimsaljook@users.noreply.github.com>
2024-07-04 14:59:44 -0400

    Update Django API Url

--- app/src/main/java/com/example/jurisscan/DocumentView.java
+++ app/src/main/java/com/example/jurisscan/DocumentView.java
@@ -270,7 +270,7 @@ public class DocumentView extends AppCompatActivity {
                     .build();
 
             Request request = new Request.Builder()
-                    .url("http://10.0.2.2:8000/upload/")
+                    .url("https://thirty-onions-hope.loca.lt/upload/")
                     .post(requestBody)
                     .build();
 
